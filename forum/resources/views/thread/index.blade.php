24ea8568d19348dae6da2f7aeb8efaed27f98d10
tohu.masao <konoemario@gmail.com>
2017-11-29 22:42:34 +0900

    tdd3 first commit

--- forum/resources/views/thread/index.blade.php
+++ forum/resources/views/thread/index.blade.php
@@ -1,9 +1,14 @@
-<h1>This is Thread!</h1>
-
-<ul>
-    @foreach($threads as $thread)
-        <li>{{ $thread->title }}</li>
-        <li>{{ $thread->body }}</li>
-        <hr/>
-    @endforeach
-</ul>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h1>This is Thread!</h1>
+    <div class="threads">
+        @foreach($threads as $thread)
+        <div class="thread">
+            <h1> {{ $thread->title }} </h1>
+            <p> {{ $thread->body }} </p>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
