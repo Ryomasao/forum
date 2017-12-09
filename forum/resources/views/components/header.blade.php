87302245a74b06ccef4092c13ad286af73ca4db5
tohu.masao <konoemario@gmail.com>
2017-12-09 09:59:24 +0900

    エラー表示を追加、画面レイアウトも変更

--- forum/resources/views/components/header.blade.php
+++ forum/resources/views/components/header.blade.php
@@ -11,6 +11,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <!--
     <link href="https://fonts.googleapis.com/earlyaccess/sawarabigothic.css" rel="stylesheet" />
+    -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
--- forum/resources/views/thread/create.blade.php
+++ forum/resources/views/thread/create.blade.php
@@ -3,26 +3,43 @@
 <div class="container justfy-center">
     <form class="form" action="/thread" method="post">
         {{ csrf_field() }}
-        <h2 class="form-header">何かを投稿しよう</h2>
-        <div class="form-group">
-            <label for="title">タイトル</label>
-            <input type="text" name="title">
-        </div>
-        <div class="form-group">
-            <label for="body">本文</label>
-            <textarea name="body" rows="10" cols="50"></textarea>
-        </div>
-        <div class="errors">
-            @if($errors->has('body'))
-                <p> {{ $errors->first('body') }} </p>
+        <h2 class="form__header">何かを投稿しよう</h2>
+        <div class="form__row">
+            <div class="form__input">
+                <label class="form__title" for="title">タイトル</label>
+                <div class="form__controls">
+                    <input class="form__control" type="text" name="title">
+                </div>
+            </div>
+            @if($errors->has('title'))
+            <div class="form__error">
+                <label class="form__notify">{{ $errors->first('title') }}</label>
+            </div>
             @endif
         </div>
-        <div class="form-group">
-            <label for="user_id">ユーザーID</label>
-            <input type="text" name="user_id" placeholder="とりあえずの実装">
+        <div class="form__row">
+            <div class="form__input">
+                <label class="form__title" for="body">本文</label>
+                <div class="form__controls">
+                    <textarea class="form__control" name="body" rows="10" cols="50"></textarea>
+                </div>
+            </div>
+            @if($errors->has('title'))
+            <div class="form__error">
+                <label class="form__notify">{{ $errors->first('body') }}</label>
+            </div>
+            @endif
         </div>
-        <div class="form-group">
-            <button>投稿する</button>
+        <div class="form__row">
+            <div class="form__input">
+                <label class="form__title" for="title">ユーザーID</label>
+                <div class="form__controls">
+                    <input class="form__control" type="text" name="user_id">
+                </div>
+            </div>
+        </div>
+        <div class="form__footer">
+            <button class="form__control">投稿する</button>
         </div>
     </form>
 
